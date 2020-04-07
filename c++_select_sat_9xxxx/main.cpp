c85901e5fc222540adbc4e2f114237220a99ce57
CharlesPhillipsHouston <charles@spaceflightresearch.net>
2020-04-07 14:13:14 -0500

    Initial Commit

---
+++ c++_select_sat_9xxxx/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c++_select_sat_9xxxx
+//
+//  Created by Charles Phillips on 4/7/20.
+//  Copyright © 2020 Charles Phillips. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
